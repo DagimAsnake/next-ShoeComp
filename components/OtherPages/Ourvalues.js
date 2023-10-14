d022daa8bf0fa1ab0b2474cf6fcff9dbc21e712d
Dagim Asnake <dagimasnake1@gmail.com>
2023-10-14 10:16:47 -1200

    added our values

--- components/OtherPages/Ourvalues.js
+++ components/OtherPages/Ourvalues.js
@@ -2,57 +2,73 @@ import { motion } from 'framer-motion';
 import { FaHeart, FaCheckCircle, FaHandshake } from 'react-icons/fa';
 
 const OurValues = () => {
-  const iconVariants = {
-    hidden: { opacity: 0, scale: 0 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        duration: 0.5,
-        delay: 0.2,
-      },
-    },
-  };
+    const iconVariants = {
+        hidden: { opacity: 0, scale: 0 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                duration: 0.5,
+                delay: 0.2,
+            },
+        },
+    };
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <motion.div
-        className="flex items-center justify-center text-6xl text-indigo-600 mb-8"
-        variants={iconVariants}
-        initial="hidden"
-        animate="visible"
-      >
-        <FaHeart />
-      </motion.div>
-      <motion.h1
-        className="text-4xl font-bold text-gray-800 mb-4"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.4 }}
-      >
-        Our Values
-      </motion.h1>
-      <motion.div
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.6 }}
-      >
-        <div className="flex items-center justify-center p-4 bg-white rounded-lg shadow-md">
-          <FaCheckCircle className="text-4xl text-green-500 mr-4" />
-          <p className="text-lg text-gray-800">Quality</p>
+    return (
+        <div className="flex flex-col items-center justify-center pb-36 bg-gray-100">
+            <motion.div
+                className="flex items-center justify-center text-6xl text-indigo-600 mb-8"
+                variants={iconVariants}
+                initial="hidden"
+                animate="visible"
+            >
+                <FaHeart />
+            </motion.div>
+            <motion.h1
+                className="text-4xl font-bold text-indigo-600 mb-4"
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.4 }}
+            >
+                Our Values
+            </motion.h1>
+            <motion.p
+                className="text-xl text-gray-600 text-center max-w-lg mb-8"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.6 }}
+            >
+                At ShoeCompany, we are committed to providing the highest quality shoes and exceptional customer service. Our values define who we are and how we operate.
+            </motion.p>
+            <motion.div
+                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 my-10"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.6 }}
+            >
+                <div className="flex items-center justify-center p-4 bg-white rounded-lg shadow-md">
+                    <FaCheckCircle className="text-4xl text-green-500 mr-4" />
+                    <p className="text-lg text-gray-800">Quality</p>
+                </div>
+                <div className="flex items-center justify-center p-4 bg-white rounded-lg shadow-md">
+                    <FaHandshake className="text-4xl text-blue-500 mr-4" />
+                    <p className="text-lg text-gray-800">Customer Satisfaction</p>
+                </div>
+                <div className="flex items-center justify-center p-4 bg-white rounded-lg shadow-md">
+                    <FaHeart className="text-4xl text-red-500 mr-4" />
+                    <p className="text-lg text-gray-800">Passion</p>
+                </div>
+                <div className="flex items-center justify-center p-4 bg-white rounded-lg shadow-md">
+                    <FaHeart className="text-4xl text-red-500 mr-4" />
+                    <p className="text-lg text-gray-800">Innovation</p>
+                </div>
+                <div className="flex items-center justify-center p-4 bg-white rounded-lg shadow-md">
+                    <FaHeart className="text-4xl text-red-500 mr-4" />
+                    <p className="text-lg text-gray-800">Integrity</p>
+                </div>
+            </motion.div>
         </div>
-        <div className="flex items-center justify-center p-4 bg-white rounded-lg shadow-md">
-          <FaHandshake className="text-4xl text-blue-500 mr-4" />
-          <p className="text-lg text-gray-800">Customer Satisfaction</p>
-        </div>
-        <div className="flex items-center justify-center p-4 bg-white rounded-lg shadow-md">
-          <FaHeart className="text-4xl text-red-500 mr-4" />
-          <p className="text-lg text-gray-800">Passion</p>
-        </div>
-      </motion.div>
-    </div>
-  );
+    );
 };
 
 export default OurValues;
--- pages/index.js
+++ pages/index.js
@@ -16,43 +16,46 @@ const Home = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center min-h-screen bg-gray-100 px-2 md:px-24">
-      <div className="flex flex-col items-center justify-center">
-        <motion.h1
-          className="text-indigo-600 text-3xl md:text-5xl font-bold text-center mb-4"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.4 }}
-        >
-          Welcome to Shoe Company
-        </motion.h1>
-        <motion.p
-          className="text-lg md:text-2xl text-gray-600 text-center max-w-md"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.6 }}
-        >
-          We offer a wide range of stylish and comfortable shoes for every occasion. Discover your perfect pair today!
-        </motion.p>
-        <motion.button
-          className="bg-indigo-600 hover:bg-indigo-700 text-white md:text-3xl text-xl font-bold py-2 px-4 rounded mt-8"
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5, delay: 0.8 }}
-        >
-          Shop Now
-        </motion.button>
-      </div>
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center py-36 bg-gray-100 px-2 md:px-24">
+        <div className="flex flex-col items-center justify-center mb-32">
+          <motion.h1
+            className="text-indigo-600 text-3xl md:text-5xl font-bold text-center mb-4"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+          >
+            Welcome to Shoe Company
+          </motion.h1>
+          <motion.p
+            className="text-lg md:text-2xl text-gray-600 text-center max-w-md"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+          >
+            We offer a wide range of stylish and comfortable shoes for every occasion. Discover your perfect pair today!
+          </motion.p>
+          <motion.button
+            className="bg-indigo-600 hover:bg-indigo-700 text-white md:text-3xl text-xl font-bold py-2 px-4 rounded mt-8"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.8 }}
+          >
+            Shop Now
+          </motion.button>
+        </div>
 
-      <motion.div
-        className="flex items-center justify-center text-9xl text-indigo-600 md:mb-8"
-        variants={iconVariants}
-        initial="hidden"
-        animate="visible"
-      >
-        <FaShoePrints />
-      </motion.div>
-    </div>
+        <motion.div
+          className="flex items-center justify-center text-9xl text-indigo-600 md:mb-8"
+          variants={iconVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <FaShoePrints />
+        </motion.div>
+      </div>
+      <OurValues />
+    </>
   );
 };
 
